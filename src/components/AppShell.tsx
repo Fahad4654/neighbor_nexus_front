479ef5f122321d86e0d502561a49c296580bd256
Fahad Kabir <kabirkaife@gmail.com>
2025-11-28 20:06:56 +0000

    the button is gone now

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -34,7 +34,7 @@ export function AppShell({ children }: { children: React.ReactNode }) {
             </p>
         </SidebarFooter>
       </Sidebar>
-      <SidebarInset className="z-0">
+      <SidebarInset>
         <header className="flex h-14 shrink-0 items-center gap-4 border-b bg-card px-4 lg:h-[60px] lg:px-6 sticky top-0 z-40">
           <SidebarTrigger className="lg:hidden" />
           <div className="w-full flex-1">
